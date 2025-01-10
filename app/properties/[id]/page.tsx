1ceca88ca0e09ee2978b06dc31dd41232236d623
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-10 16:44:35 +0100

    .

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -14,10 +14,16 @@ const PropertyDetailPage = () => {
 
             <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
                 <div className="py-6 pr-6 col-span-3">
-                    left side
+                    <h1 className="mb-4 text-4xl:">Property name</h1>
+
+                    <span className="mb-6 block text-lg text-gray-600">
+                        4 guests · 2 bedrooms · 2 beds · 2 baths
+                    </span>
                 </div>
 
-                <div className="m"
+                <div>
+                    right
+                </div>
             </div>
 
         </main>
