54c189fff121bf2ca16381f3cc773746f86b1f70
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-10 15:41:13 +0100

    property detail first commit

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+
 const PropertyDetailPage = () => {
     return (
-        <p> BLAS SADASDSA</p>
+        <main className="max-w-[1500px] mx-auto px-6">
+            <div className="w-full h-[64vh] overflow-hidden rounded-xl relative">
+                <Image 
+                    fill
+                    src="/beach_1.jpg"
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+                />
+            </div>
+
+            
+        </main>
     )
 }
 
