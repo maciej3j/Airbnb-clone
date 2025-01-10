ad24eefb34a03dafcac4482cb42545471add612a
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-01-10 14:20:40 +0100

    add categories to the site

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,8 +1,26 @@
+import Image from "next/image";
+
 const Categories = () => {
     return (
         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12 ">
-            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:opacity-100">
-                asddsa
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+                <Image src="/icn_category_beach.jpeg" width={20} height={20} alt="Category - Beach"/>
+                <span className="text-sm">Beach</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+                <Image src="/icn_category_beach.jpeg" width={20} height={20} alt="Category - Beach"/>
+                <span className="text-sm">Villas</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+                <Image src="/icn_category_beach.jpeg" width={20} height={20} alt="Category - Beach"/>
+                <span className="text-sm">Cabins</span>
+            </div>
+
+            <div className="pb-4 flex flex-col items-center space-y-2 border-b-2 border-white opacity-60 hover:border-gray-200 hover:opacity-100">
+                <Image src="/icn_category_beach.jpeg" width={20} height={20} alt="Category - Beach"/>
+                <span className="text-sm">Tiny homes</span>
             </div>
         </div>
     )
